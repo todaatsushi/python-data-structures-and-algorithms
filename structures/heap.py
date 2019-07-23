bdffe1f970d675c78aac6a8e5c5f51f5df898763
Atsushi <todaatsushi0@gmail.com>
2019-07-23 19:27:12 +0100

    Add heap get and add methods, complete adjust down

--- structures/heap.py
+++ structures/heap.py
@@ -4,8 +4,11 @@ Python implementation of the heap (min heap) data structure.
 
 
 class MinHeap:
-    def __init__(self):
-        self.nodes = []
+    def __init__(self, min=None):
+        self.nodes = [] if not min else [min]
+
+    def __str__(self):
+        return str(self.nodes)
 
     def get_child_indicies(self, index):
         """
@@ -39,18 +42,10 @@ class MinHeap:
         }
 
     def get_parent_index(self, index):
+        """
+        Gets the index of the parent node given that node's index.
+        """
         return (index - 1 ) / 2 if index != 0 else None
-
-    def get_min(self):
-        """
-        Returns the smallest (first) item in heap.
-        """
-        if len(self.nodes) == 0:
-            raise IndexError('Heap is empty!')
-        return self.nodes.pop(0)
-
-    def add_node(self, node):
-        self.nodes.append(node)
 
     def swap_nodes(self, n1_index, n2_index):
         """
@@ -89,3 +84,47 @@ class MinHeap:
         """
         index = 0
         children = self.get_child_indicies(index)
+        left = children['left']
+        right = children['right']
+
+        while left:
+            if right and self.nodes[right] < self.nodes[left]:
+                smaller_child_index = right
+            else:
+                smaller_child_index = left
+
+            if self.nodes[index] < self.nodes[smaller_child_index]:
+                self.swap_nodes(index, smaller_child_index)
+                index = smaller_child_index
+                children = self.get_child_indicies(index)
+                left = children['left']
+                right = children['right']
+
+    def get_min(self):
+        """
+        Returns the smallest (first) item in heap, adjusts the heap down after.
+        """
+        if len(self.nodes) == 0:
+            raise IndexError('Heap is empty!')
+        
+        # Remove and replace min node with last added node
+        self.swap_nodes(0, len(self.nodes) - 1)
+        min = self.nodes.pop()
+
+        self.adjust_heap_down()
+        return min
+
+    def add_node(self, node):
+        """
+        Adds a node value to the end of the heap, adjusts it up.
+        """
+        self.nodes.append(node)
+
+        self.adjust_heap_up()
+
+
+# Initialising the heap
+heap = MinHeap()
+heap2 = MinHeap(10)
+
+print(heap, heap2)
