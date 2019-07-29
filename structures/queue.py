5f68f3edda8a065d441244640a4d202537c72704
Atsushi <todaatsushi0@gmail.com>
2019-07-29 14:37:25 +0100

    Add queue implementation

---
+++ structures/queue.py
@@ -0,0 +1,34 @@
+"""
+Python implementation of a queue data structure.
+"""
+
+
+class Queue:
+    def __init__(self, *args):
+        self.vals = [val for val in args]
+    def __str__(self):
+        return str(self.vals)
+
+    def add(self, value):
+        return self.vals.append(value)
+
+    def remove(self):
+        try:
+            return self.vals.pop(0)
+        except IndexError:
+            raise IndexError('Queue is empty.')
+
+
+q = Queue()
+print(q) # []
+
+q.add(0)
+q.add(4)
+q.add(8)
+print(q) # [0, 4, 8]
+
+print(q.remove()) # 0
+print(q) # [4, 8]
+
+q2 = Queue()
+q2.remove()
