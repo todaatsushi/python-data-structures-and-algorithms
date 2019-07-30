ba5562271d9bdc88dca47b353c6ee3800b11db41
Atsushi <todaatsushi0@gmail.com>
2019-07-30 17:53:12 +0100

    Implement linear search

---
+++ algorithms/search/linear_search.py
@@ -0,0 +1,15 @@
+"""
+Linear search implementation.
+
+Given an array arr of n elements, write a function to search a given element x in arr.
+"""
+import sys
+
+def linear_search(arr, x):
+    try:
+        return arr.index(x)
+    except ValueError:
+        return -1
+
+print(linear_search([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 110))
+print(linear_search([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 175))
