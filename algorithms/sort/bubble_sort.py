3939642a53feb85fa7b8f5080e8a7ea0dba45de7
Atsushi <todaatsushi0@gmail.com>
2019-08-05 16:52:23 +0100

    Implement bubble sort skeleton

---
+++ algorithms/sort/bubble_sort.py
@@ -0,0 +1,16 @@
+"""
+Bubble Sort
+
+Sort list/array arr by repeatedly swapping wrongly ordered adjacent elements.
+"""
+
+def bubble_sort(arr, asc=True):
+    """
+    Input:
+    - list arr to be sorted
+    - asc - True to sort ascending, False for descending.
+
+    Output:
+    - returns sorted list
+    """
+    pass
