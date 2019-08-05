3b88cd6c8c3bc7e0b9443adac68beb50a07ed7b7
Atsushi <todaatsushi0@gmail.com>
2019-08-05 17:22:53 +0100

    Add comments to bubble sort

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -13,26 +13,38 @@ def bubble_sort(arr, asc=True):
     Output:
     - returns sorted list
     """
+    # Return variable
     final_list = list(arr)
+
+    # Flag variable
     no_swaps = True
 
     while True:
         for i in range(0, len(final_list)):
+            # Compare current and next values
             current = final_list[i]
+
+            # Break loop if at the end of the list
             try:
                 next = final_list[i + 1]
             except IndexError:
                 break
-            
+
+            # Swap places of the two elements if current is larger
             if current > next:
                 final_list[i] = next
                 final_list[i + 1] = current
+                
+                # Flag constraint fail
                 no_swaps = False
         
+        # No swaps means list is in order so break loop
         if no_swaps:
             break
+        # Otherwise reassume default position
         no_swaps = True
 
+    # Reverse the list if we wanted descending
     if not asc:
         return [final_list[i] for i in range(len(final_list) - 1, -1, -1)]
     
