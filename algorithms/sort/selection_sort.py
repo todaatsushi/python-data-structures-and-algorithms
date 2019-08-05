df17d0fd51ec6550e085752ea94961b31f7b173e
Atsushi <todaatsushi0@gmail.com>
2019-08-05 16:29:34 +0100

    Implement selection sort

--- algorithms/sort/selection_sort.py
+++ algorithms/sort/selection_sort.py
@@ -14,4 +14,25 @@ def selection_sort(arr, asc=True):
     Outputs:
     - Sorted version of list of ints.
     """
-    pass
+    sorted_list = []
+    unsorted_list = list(arr)
+
+    while len(unsorted_list) is not 0:
+        current = unsorted_list.pop(
+            unsorted_list.index(min(unsorted_list))
+        )
+        sorted_list.insert(len(sorted_list), current)
+
+    if not asc:
+        desc_list = []
+        for i in sorted_list:
+            desc_list.insert(0, i)
+        return desc_list
+    return sorted_list
+
+
+import random
+to_sort = [random.randint(0, 1000) for i in range(0, 10)]
+
+print(selection_sort(to_sort))
+print(selection_sort(to_sort, False))
