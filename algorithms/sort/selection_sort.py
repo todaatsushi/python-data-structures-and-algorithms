78f2ae3d34fa7c9e7719fc3858915fb48762e5cf
Atsushi <todaatsushi0@gmail.com>
2019-08-05 16:07:27 +0100

    Initial skeleton of selection sort implemetation

---
+++ algorithms/sort/selection_sort.py
@@ -0,0 +1,17 @@
+"""
+Selection Sort
+
+Sorts array/list arr by repeatedly finding the min element from the unsorted section of
+arr, and putting it at the beginning.
+"""
+
+def selection_sort(arr, asc=True):
+    """
+    Input:
+    - Unsorted list of ints
+    - Bool indicating ascending (True) or descending (False)
+
+    Outputs:
+    - Sorted version of list of ints.
+    """
+    pass
