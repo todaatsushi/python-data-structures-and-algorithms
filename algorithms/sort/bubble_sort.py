8745eeadd0145f97f5a99c6b2c9a3485a488ab52
Atsushi <todaatsushi0@gmail.com>
2019-08-05 17:20:29 +0100

    Implement bubble sort in python

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -7,10 +7,39 @@ Sort list/array arr by repeatedly swapping wrongly ordered adjacent elements.
 def bubble_sort(arr, asc=True):
     """
     Input:
-    - list arr to be sorted
+    - list arr to be sorted (assumes list has at least 2 elements)
     - asc - True to sort ascending, False for descending.
 
     Output:
     - returns sorted list
     """
-    pass
+    final_list = list(arr)
+    no_swaps = True
+
+    while True:
+        for i in range(0, len(final_list)):
+            current = final_list[i]
+            try:
+                next = final_list[i + 1]
+            except IndexError:
+                break
+            
+            if current > next:
+                final_list[i] = next
+                final_list[i + 1] = current
+                no_swaps = False
+        
+        if no_swaps:
+            break
+        no_swaps = True
+
+    if not asc:
+        return [final_list[i] for i in range(len(final_list) - 1, -1, -1)]
+    
+    return final_list
+
+import random
+arr = [random.randint(0, 1000) for i in range(0, 6)]
+
+print(bubble_sort(arr, True))
+print(bubble_sort(arr, False))
