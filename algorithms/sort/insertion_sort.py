e485a91591369426b36a5271b1dfca328eba3adb
Atsushi <todaatsushi0@gmail.com>
2019-08-06 17:33:46 +0100

    Basic skeleton for insertion sort

---
+++ algorithms/sort/insertion_sort.py
@@ -0,0 +1,20 @@
+"""
+Insertion sort.
+
+Sort list/array arr of ints and sort it by iterating through and placing each element
+where it should be.
+"""
+
+def insertion_sort(arr, asc=True):
+    """
+    Inputs:
+    - Arr - list of ints to be sorted
+    - asc - True for ascending, False for descending
+    """
+    pass
+
+import random
+unsorted = [random.randin(0, 1000) for i in range(0, 10)]
+
+print(insertion_sort(unsorted))
+print(insertion_sort(unsorted, False))
