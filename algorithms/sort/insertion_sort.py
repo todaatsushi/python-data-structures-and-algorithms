6a4b19dfee13fbe1212036cab7c598e78592c71a
Atsushi <todaatsushi0@gmail.com>
2019-08-06 17:53:51 +0100

    Implement insertion sort

--- algorithms/sort/insertion_sort.py
+++ algorithms/sort/insertion_sort.py
@@ -8,13 +8,34 @@ where it should be.
 def insertion_sort(arr, asc=True):
     """
     Inputs:
-    - Arr - list of ints to be sorted
+    - Arr - list of ints to be sorted. Assumes arr
+    is longer than 1 element long.
     - asc - True for ascending, False for descending
     """
-    pass
+    final = list(arr)
+
+    # Iterate through list
+    for i in range(1, len(final)):
+        current = final[i]
+
+        # Compare current to every preceding element
+        for n in range(0, i):
+
+            # If smaller, place before and remove current from current location
+            if current < final[n]:
+                final.pop(i)
+                final.insert(n, current)
+                break
+
+    # Reverse list for descending order
+    if not asc:
+        return [final[i] for i in range(len(final) - 1, -1, -1)]
+
+    return final
+
 
 import random
-unsorted = [random.randin(0, 1000) for i in range(0, 10)]
+unsorted = [random.randint(0, 1000) for i in range(0, 10)]
 
 print(insertion_sort(unsorted))
 print(insertion_sort(unsorted, False))
