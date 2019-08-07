1ead05c9b7bfdd9d5940f350cf3be718c9952fa9
Atsushi <todaatsushi0@gmail.com>
2019-08-07 15:29:13 +0100

    Implement merge sort skeleton

---
+++ algorithms/sort/merge_sort.py
@@ -0,0 +1,17 @@
+"""
+Merge sort
+
+Divides array/list arr into 2 halves, sorts the two halves, and then
+merges the two sorted halves.
+"""
+
+def merge_sort(arr, asc=True):
+    """
+    """
+    pass
+
+import random
+unsorted = [random.randint(0, 1000) for i in range(0, 10)]
+
+print(merge_sort(unsorted))
+print(merge_sort(unsorted, False))
