42f50479f6ff7cab64ee1505c4ce46f92ba37f5d
Atsushi <todaatsushi0@gmail.com>
2019-08-08 18:26:48 +0100

    Implemented ascending basic working quick sort

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,14 +1,12 @@
 """
 Quick sort
 
-Sort list/array arr by partitioning all elems in arr around
-a partition element. 
+Sort list/array arr by recursively partitioning all elems in arr around
+a pivot element. 
 """
 
 def quick_sort(arr, asc=True):
     """
-    Always uses the first element as a pivot.
-
     Input:
     - arr - list of ints to sort
     - asc - bool. True for ascending, False for descending
@@ -16,25 +14,64 @@ def quick_sort(arr, asc=True):
     Outputs:
     - List of sorted ints
     """
-    # base case - if arr is 1 element long, return element
-    # Pick pivot element
-    # get endpoints of array
-    
-    # while left and right haven't met
-        # Move left and right points inwards until left element is larger
-        # than the pivot elem and right element is smaller
-        # Swap values
-     
-     # split arr where left and right have met
-     # call quick_sort on each half
+    size = len(arr)
+    pivot = arr[0]
 
-     # flip if descending
-     # return
-    pass
+    # Base case
+    if size == 1:
+        return arr
+
+    left, right = 0, size -1
+    no_swap = False
+
+    while left < right:
+        # find left
+        # Increment left until we find an element greater than pivot
+        while not arr[left] > pivot:
+            left += 1
+            if left == size:
+                break
+
+        # decremenet right until element is smaller than or equal to pivot
+        while not arr[right] <= pivot:
+            right -= 1
+            if right == 0:
+                if left == size:
+                    no_swap = True
+                break
+
+        if no_swap:
+            no_swap = False
+        elif left > right:
+            break
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+
+    # Swap pivot right right elem
+    arr[0], arr[right] = arr[right], arr[0]
+    
+    # split arr at new pivot location i.e. right
+    if size == 2:
+        pass
+    else:
+        split_point = arr.index(arr[right])
+        arr[0:right] = quick_sort(arr[0:right])
+        arr[right + 1:size + 1] = quick_sort(arr[right + 1:size + 1])
+
+    return arr
+        
+            
+
+    
+                
 
 
 import random
-unsorted = [random.randint(0, 1000) for i in range(0, 9)]
+# unsorted = [15, 3, 9, 8, 5, 2, 7, 1, 6]
+unsorted = [10, 16, 8, 12, 15, 6, 3, 9, 5]
 
-print(quick_sort(unsorted))
-print(quick_sort(unsorted, False))
+# unsorted = [random.randint(0, 20) for i in range(0, 6)]
+quick = quick_sort(unsorted)
+print(quick)
+
+# print(quick_sort(unsorted, False))
