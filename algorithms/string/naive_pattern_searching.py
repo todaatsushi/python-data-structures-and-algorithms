e172899d9ec99622fe1da915d17500ff9f205729
Atsushi <todaatsushi0@gmail.com>
2019-08-09 16:24:17 +0100

    Implement algorithm

--- algorithms/string/naive_pattern_searching.py
+++ algorithms/string/naive_pattern_searching.py
@@ -15,7 +15,33 @@ def naive_pattern_match(txt, pat):
     - List of ints, where each int indicates where in txt pat
     appears.
     """
-    pass
+    if not pat:
+        raise ValueError('Pattern must not be empty!')
+    # Get intial variables
+    indicies = []
+    first_letter = pat[0]
+
+    # Cycle through txt
+    for n in range(0, len(txt)):
+        # compare current letter with first letter to pattern match
+        current = txt[n]
+        if current == first_letter:
+            match = True
+            # If they match, cycle through the rest of the pattern
+            for i in range(0, len(pat)):
+                # Break if letters don't match / if we run out of word
+                try:
+                    if pat[i] != txt[n + i]:
+                        match = False
+                        break
+                except IndexError:
+                    match = False
+                    break
+            # if flag is still true, add the initial index to the list
+            if match:
+                indicies.append(n)
+    return indicies
+
 
 txt = 'AABAACAADAABAABA'
 pat = 'AABA'
