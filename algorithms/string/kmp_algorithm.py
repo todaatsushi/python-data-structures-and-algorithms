fb6f9564c669a1c7d30a09d8f785906c74862427
Atsushi <todaatsushi0@gmail.com>
2019-08-12 12:05:05 +0100

    Add pseudocode

--- algorithms/string/kmp_algorithm.py
+++ algorithms/string/kmp_algorithm.py
@@ -16,6 +16,25 @@ def kmp_search(txt, pat):
 
     Returns empty string if pat doesn't exist.
     """
+    # Create pi table - all vals to set to 0 initially
+    ## brute force search for repeating prefix in the suffix
+    # Loop over pat
+        # if letter matches first letter of pat, change value to 1
+        # if next letter matches the next letter of pat, change value to 2
+        # continue until mismatch
+    # add a start point at the beginning of the pi table
+
+    ## Algorithm
+    # Loop over txt
+        # If letter matches first letter in pat
+        # Log starting index
+            # Check next letter
+            # Continue until mismatch
+        ## Mismatch
+            # revert pattern index to the value index assigned to the
+            # letter in the pi table
+    # if word is matched, add starting index to return list
+    # else return empty list
     pass
 
 
