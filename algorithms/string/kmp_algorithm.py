c1d093bbe82dfe10ff351df2c14d26727f74418c
Atsushi <todaatsushi0@gmail.com>
2019-08-12 16:15:11 +0100

    Implement pi table

--- algorithms/string/kmp_algorithm.py
+++ algorithms/string/kmp_algorithm.py
@@ -5,6 +5,7 @@ Searches string txt for string pat but avoid wasted time by mapping repeating pa
 in pat and creating a pi table to avoid rematching already matched portions of txt.
 """
 
+
 def kmp_search(txt, pat):
     """
     Input:
@@ -17,12 +18,37 @@ def kmp_search(txt, pat):
     Returns empty string if pat doesn't exist.
     """
     # Create pi table - all vals to set to 0 initially
+    pi = [
+        {
+            'letter': pat[i],
+            'value': 0
+        } for i in range(len(pat))
+    ]
     ## brute force search for repeating prefix in the suffix
     # Loop over pat
-        # if letter matches first letter of pat, change value to 1
-        # if next letter matches the next letter of pat, change value to 2
+    for n in range(1, len(pat)):
+        value_counter = 1
+        index = n
+
+        # loop over pattern find matches to prefix of pattern
+        # increment the value as number of matches increases
         # continue until mismatch
+        for i in range(len(pat)):
+            if pat[index] == pat[i]:
+                pi[index]['value'] = value_counter
+                value_counter += 1
+                index += 1
+            else:
+                value_counter = 1
+                index = n
+                break
+
     # add a start point at the beginning of the pi table
+    pi.insert(0, 'start')
+    print(pi)
+
+    # Return object
+    indicies = []
 
     ## Algorithm
     # Loop over txt
